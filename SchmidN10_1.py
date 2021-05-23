9cd1df49b4a3a572360e4f23c157cab46d4d1274
TheRetroNick <84204853+TheRetroNick@users.noreply.github.com>
2021-05-23 13:37:14 -0600

    Add files via upload

--- SchmidN10_1.py
+++ SchmidN10_1.py
@@ -1,6 +1,28 @@
-
+# Import OS Mod
 import os
+import time
 
+#print Current Directory
 cwd = os.getcwd()
 print("Current Working Directory:" ,cwd)
+# Print directory list
+listdir = os.listdir()
+print('Directory List:', listdir)
+# Take user input to change the directory
+os.chdir(input('What directory do you want to save in? '))
+# Print the new CWD
+cwd = os.getcwd()
+print("Current Working Directory:" ,cwd)
+# Start creating a file
+New = input("New file name? ")
+# Input a file name
 
+fh = open((New) +'.txt', 'w')
+fh.write(input('What is your name? '))
+fh.write(', ')
+fh.write(input('Address? '))
+fh.write(', ')
+fh.write(input('Phone number? '))
+fh.close()
+print("File created - " +(New))
+time.sleep(3)
